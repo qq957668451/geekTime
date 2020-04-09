99642dbdcbd7499aee1558b4dfb22590ffd4fa95
zch <957668451@qq.com>
2020-04-09 18:16:30 +0800

    second

---
+++ src/pages/shopping/production.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h2>产品</h2>
+    <ul>
+      <li v-for="item in production" :key="item.id">
+        <div>{{ item.name }}</div>
+        <div><button @click="addShopCar(item)">加入购物车</button></div>
+      </li>
+    </ul>
+  </div>
+</template>
+<script lang="ts">
+import Vue from "vue";
+export default class App extends Vue {
+  get production() {
+    return this.$store.state.production;
+  }
+  addShopCar(pro: any) {
+    console.log("pro", pro);
+  }
+}
+</script>
+
+<style lang="less" scoped></style>
---
+++ src/pages/shopping/shoppingList.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <div>hello</div>
+  </div>
+</template>
+<script lang="ts">
+import Vue from "vue";
+
+export default class App extends Vue {
+  msg = 123;
+  mounted() {} //计算属性
+  get computedMsg() {
+    return "";
+  } //方法
+  greet() {}
+}
+</script>
+
+<style lang="less" scoped></style>
